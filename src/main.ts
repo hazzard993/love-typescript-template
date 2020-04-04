83e505291b1b546cfe13bf062f86c2f1396d9e02
hazzard993 <hazzard993@gmail.com>
2020-04-04 15:42:08 +1000

    Fix linting issues

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,8 @@
-love.load = args => {
+love.load = () => {
     const [content] = love.filesystem.read("res/index.txt");
     print(content);
 };
 
-love.update = dt => {};
-
 love.draw = () => {
     love.graphics.print("Hello World!", 400, 300);
-    draw;
-    translate;
-    physics;
 };
